204e8821024f4b10690435ad89424392d0793e83
Nicolas Menard <nicolas.p.menard@gmail.com>
2022-11-30 12:47:26 -0500

    update build process

--- build.rs
+++ build.rs
@@ -1,7 +1,44 @@
 use anyhow::{anyhow, Context};
 use const_gen::*;
+use serde::{Deserialize, Serialize};
 use std::{env, fs, path::Path};
 use tokio::runtime::Runtime;
+use viam::gen::proto::app::v1::{robot_service_client::RobotServiceClient, CertificateRequest};
+use viam_rust_utils::rpc::dial::{DialOptions, RPCCredentials};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub cloud: Cloud,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Cloud {
+    pub id: String,
+    pub secret: String,
+    #[serde(default)]
+    pub location_secret: String,
+    #[serde(default)]
+    managed_by: String,
+    #[serde(default)]
+    fqdn: String,
+    #[serde(default)]
+    local_fqdn: String,
+    #[serde(default)]
+    signaling_address: String,
+    #[serde(default)]
+    signaling_insecure: bool,
+    #[serde(default)]
+    path: String,
+    #[serde(default)]
+    log_path: String,
+    app_address: String,
+    #[serde(default)]
+    refresh_interval: String,
+    #[serde(default)]
+    pub tls_certificate: String,
+    #[serde(default)]
+    pub tls_private_key: String,
+}
 
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
 fn main() -> anyhow::Result<()> {
@@ -23,9 +60,11 @@ fn main() -> anyhow::Result<()> {
     }
 
     let content = std::fs::read_to_string("viam.json").context("can't read viam.json")?;
-    let mut cfg = viam::config::reader::Config::new(content.as_str())?;
+
+    let mut cfg: Config = serde_json::from_str(content.as_str()).map_err(anyhow::Error::msg)?;
+
     let rt = Runtime::new()?;
-    rt.block_on(cfg.read_certificates());
+    rt.block_on(read_certificates(&mut cfg))?;
     let out_dir = std::env::var_os("OUT_DIR").unwrap();
     let dest_path = std::path::Path::new(&out_dir).join("ca.crt");
     let ca_cert = String::from(&cfg.cloud.tls_certificate) + "\0";
@@ -51,4 +90,26 @@ fn main() -> anyhow::Result<()> {
 
     embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
     embuild::build::LinkArgs::output_propagated("ESP_IDF")
+}
+
+async fn read_certificates(config: &mut Config) -> anyhow::Result<()> {
+    let creds = RPCCredentials::new(
+        Some(config.cloud.id.clone()),
+        "robot-secret".to_string(),
+        config.cloud.secret.clone(),
+    );
+    let cert_req = CertificateRequest {
+        id: config.cloud.id.clone(),
+    };
+    let dial = DialOptions::builder()
+        .uri(config.cloud.app_address.clone().as_str())
+        .with_credentials(creds)
+        .disable_webrtc()
+        .connect()
+        .await?;
+    let mut app_service = RobotServiceClient::new(dial);
+    let certs = app_service.certificate(cert_req).await?.into_inner();
+    config.cloud.tls_certificate = certs.tls_certificate;
+    config.cloud.tls_private_key = certs.tls_private_key;
+    Ok(())
 }
