9cf8561de6274709789d500f54cabf285638de72
Gautham Varadarajan <gautham@viam.com>
2023-04-12 15:31:39 -0400

    remove polling thread and cleanup

--- src/common/mpu6050.rs
+++ src/common/mpu6050.rs
@@ -14,10 +14,7 @@ use super::status::Status;
 use byteorder::{BigEndian, ReadBytesExt};
 use std::collections::BTreeMap;
 use std::io::Cursor;
-use std::sync::mpsc::{self, Sender, TryRecvError};
 use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
 
 // This module represents an implementation of the MPU-6050 gyroscope/accelerometer
 // as a Movement Sensor component
@@ -31,126 +28,21 @@ pub(crate) fn register_models(registry: &mut ComponentRegistry) {
     }
 }
 
-// This is a struct meant to hold the most recently polled readings value
-// and the most recent success or error state in acquiring readings. This
-// is internal to this driver and should be accessed via Mutex.
-#[derive(Clone, Debug)]
-struct MPU6050State {
-    angular_velocity: Vector3,
-    linear_acceleration: Vector3,
-    // temperature: f64,
-    error: Option<Arc<anyhow::Error>>,
-}
-
-impl MPU6050State {
-    fn new() -> Self {
-        MPU6050State {
-            angular_velocity: Vector3::new(),
-            linear_acceleration: Vector3::new(),
-            // temperature: 0.0,
-            error: None,
-        }
-    }
-
-    fn get_angular_velocity(&self) -> Vector3 {
-        self.angular_velocity
-    }
-
-    fn get_linear_acceleration(&self) -> Vector3 {
-        self.linear_acceleration
-    }
-
-    fn set_error(&mut self, err: Option<Arc<anyhow::Error>>) {
-        self.error = err;
-    }
-
-    fn set_linear_acceleration_from_reading(&mut self, reading: &[u8; 14]) {
-        let mut slice_copy = vec![0; 6];
-        slice_copy.clone_from_slice(&(reading[0..6]));
-        let mut rdr = Cursor::new(slice_copy);
-        let unscaled_x = rdr.read_i16::<BigEndian>().unwrap();
-        let unscaled_y = rdr.read_i16::<BigEndian>().unwrap();
-        let unscaled_z = rdr.read_i16::<BigEndian>().unwrap();
-
-        let max_acceleration: f64 = 2.0 * 9.81 * 1000.0;
-
-        let x = f64::from(unscaled_x) * max_acceleration / 32768.0;
-        let y = f64::from(unscaled_y) * max_acceleration / 32768.0;
-        let z = f64::from(unscaled_z) * max_acceleration / 32768.0;
-        self.linear_acceleration = Vector3 { x, y, z };
-    }
-
-    fn set_angular_velocity_from_reading(&mut self, reading: &[u8; 14]) {
-        let mut slice_copy = vec![0; 6];
-        slice_copy.clone_from_slice(&(reading[8..14]));
-        let mut rdr = Cursor::new(slice_copy);
-        let unscaled_x = rdr.read_u16::<BigEndian>().unwrap();
-        let unscaled_y = rdr.read_u16::<BigEndian>().unwrap();
-        let unscaled_z = rdr.read_u16::<BigEndian>().unwrap();
-        let x = f64::from(unscaled_x) * 250.0 / 32768.0;
-        let y = f64::from(unscaled_y) * 250.0 / 32768.0;
-        let z = f64::from(unscaled_z) * 250.0 / 32768.0;
-        self.angular_velocity = Vector3 { x, y, z };
-    }
-}
+const reading_start_register: u8 = 59;
+const standby_mode_register: u8 = 107;
 
 pub struct MPU6050 {
-    state: Arc<Mutex<MPU6050State>>,
     i2c_handle: I2cHandleType,
     i2c_address: u8,
-    canceller: Sender<bool>,
 }
 
 impl MPU6050 {
     pub fn new(mut i2c_handle: I2cHandleType, i2c_address: u8) -> anyhow::Result<Self> {
-        let bytes: [u8; 2] = [107, 0];
+        let bytes: [u8; 2] = [standby_mode_register, 0];
         i2c_handle.write_i2c(i2c_address, &bytes)?;
-        let i2c_address_copy = i2c_address;
-        let raw_state = MPU6050State::new();
-        let (canceller, rx) = mpsc::channel();
-        let state = Arc::new(Mutex::new(raw_state));
-        // reference copies for sending memory into thread
-        let mut i2c_handle_copy = Arc::clone(&i2c_handle);
-        let state_copy = Arc::clone(&state);
-        // start a polling thread that reads from the MPU every millisecond and mutates state.
-        // This allows multi-read access to the state for the functions satisfying the
-        // Movement Sensor API
-        thread::spawn(move || loop {
-            thread::sleep(Duration::from_millis(1));
-            match rx.try_recv() {
-                Ok(_) | Err(TryRecvError::Disconnected) => {
-                    log::debug!("MPU6050: Terminating polling thread.");
-                    break;
-                }
-                Err(TryRecvError::Empty) => {
-                    let register_write: [u8; 1] = [59];
-                    let mut result: [u8; 14] = [0; 14];
-                    let mut internal_state = state_copy.lock().unwrap();
-                    let res = i2c_handle_copy.write_read_i2c(
-                        i2c_address_copy,
-                        &register_write,
-                        &mut result,
-                    );
-
-                    match res {
-                        Ok(_) => {
-                            internal_state.set_linear_acceleration_from_reading(&result);
-                            internal_state.set_angular_velocity_from_reading(&result);
-                            internal_state.set_error(None);
-                        }
-                        Err(err) => {
-                            log::error!("MPU I2C error: {:?}", err);
-                            internal_state.set_error(Some(Arc::new(err)));
-                        }
-                    };
-                }
-            }
-        });
         Ok(MPU6050 {
-            state,
             i2c_handle,
             i2c_address,
-            canceller,
         })
     }
 
@@ -190,15 +82,8 @@ impl MPU6050 {
     }
 
     pub fn close(&mut self) -> anyhow::Result<()> {
-        // close the polling thread
-        if let Err(err) = self.canceller.send(true) {
-            return Err(anyhow::anyhow!(
-                "mpu6050 failed to close polling thread: {:?}",
-                err
-            ));
-        };
         // put the MPU in the sleep state
-        let off_data: [u8; 2] = [107, 64];
+        let off_data: [u8; 2] = [standby_mode_register, 64];
         if let Err(err) = self.i2c_handle.write_i2c(self.i2c_address, &off_data) {
             return Err(anyhow::anyhow!("mpu6050 sleep command failed: {:?}", err));
         };
@@ -206,7 +91,7 @@ impl MPU6050 {
     }
 }
 
-// we want to close the MPU (terminate the polling thread and put the sensor to sleep)
+// we want to close the MPU (put the sensor to sleep)
 // when the component memory gets dropped
 impl Drop for MPU6050 {
     fn drop(&mut self) {
@@ -214,6 +99,40 @@ impl Drop for MPU6050 {
             log::error!("mpu6050 close failure: {:?}", err)
         };
     }
+}
+
+fn get_angular_velocity_from_reading(reading: [u8; 14]) -> Vector3 {
+    let mut slice_copy = vec![0; 6];
+    slice_copy.clone_from_slice(&(reading[8..14]));
+    let mut rdr = Cursor::new(slice_copy);
+    let unscaled_x = rdr.read_u16::<BigEndian>().unwrap();
+    let unscaled_y = rdr.read_u16::<BigEndian>().unwrap();
+    let unscaled_z = rdr.read_u16::<BigEndian>().unwrap();
+
+    let max_velocity: f64 = 250.0;
+    let max_u16: f64 = 32768.0;
+
+    let x = f64::from(unscaled_x) * max_velocity / max_u16;
+    let y = f64::from(unscaled_y) * max_velocity / max_u16;
+    let z = f64::from(unscaled_z) * max_velocity / max_u16;
+    Vector3 { x, y, z }
+}
+
+fn get_linear_acceleration_from_reading(reading: [u8; 14]) -> Vector3 {
+    let mut slice_copy = vec![0; 6];
+    slice_copy.clone_from_slice(&(reading[0..6]));
+    let mut rdr = Cursor::new(slice_copy);
+    let unscaled_x = rdr.read_i16::<BigEndian>().unwrap();
+    let unscaled_y = rdr.read_i16::<BigEndian>().unwrap();
+    let unscaled_z = rdr.read_i16::<BigEndian>().unwrap();
+
+    let max_acceleration: f64 = 2.0 * 9.81 * 1000.0;
+    let max_u16: f64 = 32768.0;
+
+    let x = f64::from(unscaled_x) * max_acceleration / max_u16;
+    let y = f64::from(unscaled_y) * max_acceleration / max_u16;
+    let z = f64::from(unscaled_z) * max_acceleration / max_u16;
+    Vector3 { x, y, z }
 }
 
 impl MovementSensor for MPU6050 {
@@ -227,37 +146,31 @@ impl MovementSensor for MPU6050 {
         }
     }
 
-    fn get_angular_velocity(&self) -> anyhow::Result<Vector3> {
-        let state = self.state.lock().unwrap();
-        match &state.error {
-            None => Ok(state.get_angular_velocity()),
-            Some(error_arc) => {
-                let inner_err = error_arc.as_ref();
-                Err(anyhow::anyhow!("{}", *inner_err))
-            }
-        }
+    fn get_angular_velocity(&mut self) -> anyhow::Result<Vector3> {
+        let register_write: [u8; 1] = [reading_start_register];
+        let mut result: [u8; 14] = [0; 14];
+        self.i2c_handle
+            .write_read_i2c(self.i2c_address, &register_write, &mut result)?;
+        Ok(get_angular_velocity_from_reading(result))
     }
 
-    fn get_linear_acceleration(&self) -> anyhow::Result<Vector3> {
-        let state = self.state.lock().unwrap();
-        match &state.error {
-            None => Ok(state.get_linear_acceleration()),
-            Some(error_arc) => {
-                let inner_err = error_arc.as_ref();
-                Err(anyhow::anyhow!("{}", *inner_err))
-            }
-        }
+    fn get_linear_acceleration(&mut self) -> anyhow::Result<Vector3> {
+        let register_write: [u8; 1] = [reading_start_register];
+        let mut result: [u8; 14] = [0; 14];
+        self.i2c_handle
+            .write_read_i2c(self.i2c_address, &register_write, &mut result)?;
+        Ok(get_linear_acceleration_from_reading(result))
     }
 
-    fn get_position(&self) -> anyhow::Result<super::movement_sensor::GeoPosition> {
+    fn get_position(&mut self) -> anyhow::Result<super::movement_sensor::GeoPosition> {
         anyhow::bail!("unimplemented: movement_sensor_get_position")
     }
 
-    fn get_linear_velocity(&self) -> anyhow::Result<Vector3> {
+    fn get_linear_velocity(&mut self) -> anyhow::Result<Vector3> {
         anyhow::bail!("unimplemented: movement_sensor_get_linear_velocity")
     }
 
-    fn get_compass_heading(&self) -> anyhow::Result<f64> {
+    fn get_compass_heading(&mut self) -> anyhow::Result<f64> {
         anyhow::bail!("unimplemented: movement_sensor_get_compass_heading")
     }
 }
@@ -272,23 +185,12 @@ impl Status for MPU6050 {
 
 #[cfg(test)]
 mod tests {
-    use super::MPU6050State;
+    use super::{get_angular_velocity_from_reading, get_linear_acceleration_from_reading};
 
     #[test_log::test]
     fn test_read_linear_acceleration() -> anyhow::Result<()> {
-        let mut state = MPU6050State::new();
         let reading: [u8; 14] = [64, 0, 32, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        let expected_reading: [u8; 14] = [64, 0, 32, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        state.set_linear_acceleration_from_reading(&reading);
-
-        // test reading array unchanged
-        assert_eq!(reading.len(), expected_reading.len());
-        assert!(reading
-            .iter()
-            .zip(expected_reading.iter())
-            .all(|(a, b)| a == b));
-
-        let lin_acc = state.get_linear_acceleration();
+        let lin_acc = get_linear_acceleration_from_reading(reading);
         assert_eq!(lin_acc.x, 9810.0);
         assert_eq!(lin_acc.y, 4905.0);
         assert_eq!(lin_acc.z, 2452.5);
@@ -297,52 +199,11 @@ mod tests {
 
     #[test_log::test]
     fn test_read_angular_velocity() -> anyhow::Result<()> {
-        let mut state = MPU6050State::new();
         let reading: [u8; 14] = [0, 0, 0, 0, 0, 0, 0, 0, 64, 0, 32, 0, 16, 0];
-        let expected_reading: [u8; 14] = [0, 0, 0, 0, 0, 0, 0, 0, 64, 0, 32, 0, 16, 0];
-
-        state.set_angular_velocity_from_reading(&reading);
-
-        // test reading array unchanged
-        assert_eq!(reading.len(), expected_reading.len());
-        assert!(reading
-            .iter()
-            .zip(expected_reading.iter())
-            .all(|(a, b)| a == b));
-
-        let ang_vel = state.get_angular_velocity();
+        let ang_vel = get_angular_velocity_from_reading(reading);
         assert_eq!(ang_vel.x, 125.0);
         assert_eq!(ang_vel.y, 62.5);
         assert_eq!(ang_vel.z, 31.25);
-        Ok(())
-    }
-
-    #[test_log::test]
-    fn test_multiple_values_from_single_reading() -> anyhow::Result<()> {
-        let mut state = MPU6050State::new();
-        let reading: [u8; 14] = [64, 0, 32, 0, 16, 0, 0, 0, 64, 0, 32, 0, 16, 0];
-        let expected_reading: [u8; 14] = [64, 0, 32, 0, 16, 0, 0, 0, 64, 0, 32, 0, 16, 0];
-
-        state.set_angular_velocity_from_reading(&reading);
-        state.set_linear_acceleration_from_reading(&reading);
-
-        // test reading array unchanged
-        assert_eq!(reading.len(), expected_reading.len());
-        assert!(reading
-            .iter()
-            .zip(expected_reading.iter())
-            .all(|(a, b)| a == b));
-
-        let lin_acc = state.get_linear_acceleration();
-        assert_eq!(lin_acc.x, 9810.0);
-        assert_eq!(lin_acc.y, 4905.0);
-        assert_eq!(lin_acc.z, 2452.5);
-
-        let ang_vel = state.get_angular_velocity();
-        assert_eq!(ang_vel.x, 125.0);
-        assert_eq!(ang_vel.y, 62.5);
-        assert_eq!(ang_vel.z, 31.25);
-
         Ok(())
     }
 }
