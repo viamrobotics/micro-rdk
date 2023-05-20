2e6883773810207b3307ffa1fbc0fd2b437f1d5a
Nicolas Menard <nicolas.p.menard@gmail.com>
2023-05-20 14:05:18 -0400

    update sctp

--- src/common/webrtc/io.rs
+++ src/common/webrtc/io.rs
@@ -230,7 +230,7 @@ pub struct WebRtcTransport {
 
 impl WebRtcTransport {
     pub fn new(socket: UdpSocket) -> Self {
-        let (tx, rx) = smol::channel::bounded::<IoPkt>(3);
+        let (tx, rx) = smol::channel::bounded::<IoPkt>(10);
         Self {
             socket,
             stun: IoPktChannel::new(tx.clone()),
