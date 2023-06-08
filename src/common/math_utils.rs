a49e3304598558b325295a8f75719090d95605de
Matthew J Perez <matt@mperez.io>
2023-06-08 15:07:29 -0400

    add some tests, need to validate math

--- src/common/math_utils.rs
+++ src/common/math_utils.rs
@@ -41,11 +41,8 @@ pub(crate) fn go_for_math(
     revolutions: f64,
 ) -> anyhow::Result<(f64, Option<Duration>)> {
     let rpm = rpm.clamp(-1.0, 1.0);
-    if rpm.is_nan() {
-        bail!("supplied rpm is NaN");
-    }
-    if revolutions.is_nan() {
-        bail!("supplied revolutions is NaN");
+    if max_rpm.is_nan() || rpm.is_nan() || revolutions.is_nan() {
+        bail!("NaN in supplied input");
     }
 
     if revolutions == 0.0 {
@@ -57,4 +54,51 @@ pub(crate) fn go_for_math(
     let dur = Duration::from_secs_f64((revolutions / rpm).abs() * 60.0);
 
     Ok((pct, Some(dur)))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::common::math_utils::*;
+    use std::time::Duration;
+
+    #[test_log::test]
+    fn test_go_for_math_nans() {
+        let max_rpm = 0.0;
+        let rpm = 0.0;
+        let revolutions = 0.0;
+
+        let max_nan = go_for_math(f64::NAN, rpm, revolutions);
+        assert!(max_nan.is_err());
+        let rpm_nan = go_for_math(max_rpm, f64::NAN, revolutions);
+        assert!(rpm_nan.is_err());
+        let rev_nan = go_for_math(max_rpm, rpm, f64::NAN);
+        assert!(rev_nan.is_err());
+    }
+
+    // TODO: put real inputs and expected outcomes
+
+    #[test_log::test]
+    fn test_go_for_math_none_duration() -> anyhow::Result<()>{
+        let max_rpm = 200.0;
+        let rpm = 500.0;
+        let revolutions = 0.0;
+
+        let (pwr, dur) = go_for_math(max_rpm, rpm, revolutions)?;
+        assert!(dur.is_none());
+        assert_eq!(pwr, 0.005);
+        Ok(())
+    }
+
+    #[test_log::test]
+    fn test_go_for_math_some_duration() -> anyhow::Result<()>{
+        let max_rpm = 50.0;
+        let rpm = 10.0;
+        let revolutions = 100.0;
+
+        let (pwr, dur) = go_for_math(max_rpm, rpm, revolutions)?;
+        assert_eq!(pwr, 0.02);
+        assert_eq!(dur, Some(Duration::from_secs(6000)));
+        assert!(dur.is_some());
+        Ok(())
+    }
 }
