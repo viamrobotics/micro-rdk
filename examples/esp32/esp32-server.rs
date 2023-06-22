ec4653859e91db9fa80e66e65181ab3f4436ceaa
Matthew J Perez <matt@mperez.io>
2023-06-22 14:53:05 -0400

    wip

--- examples/esp32/esp32-server.rs
+++ examples/esp32/esp32-server.rs
@@ -7,72 +7,74 @@ const PASS: &str = env!("MICRO_RDK_WIFI_PASSWORD");
 
 include!(concat!(env!("OUT_DIR"), "/robot_secret.rs"));
 
+use log::*;
+#[allow(unused_imports)]
+use std::{
+    collections::HashMap,
+    net::Ipv4Addr,
+    time::Duration,
+    sync::{Arc, Mutex},
+};
+
+// micro-rdk
+#[allow(unused_imports)]
+use micro_rdk::common::{
+    app_client::AppClientConfig,
+    robot::{LocalRobot, ResourceMap, ResourceType},
+    board::FakeBoard,
+};
+#[allow(unused_imports)]
+use micro_rdk::proto::common::v1::ResourceName;
+
+#[allow(unused_imports)]
+use micro_rdk::esp32::{
+    certificate::WebRtcCertificate, dtls::Esp32Dtls, entry::serve_web, exec::Esp32Executor,
+    tcp::Esp32Stream, tls::Esp32Tls, tls::Esp32TlsServerConfig,
+};
+
+/* webhook
+use embedded_svc::{
+    http::{client::Client as HttpClient, Method, Status},
+    io::{Read, Write},
+    utils::io,
+};
+*/
+
+// esp_idf_svc
+
+#[allow(unused_imports)]
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    eth::{EspEth, EthDriver},
+    http::client::{Configuration, EspHttpConnection},
+    netif::{EspNetif, NetifStatus, AsyncNetif}, // EspNetifWait
+    timer::EspTimerService,
+                     //wifi::WifiWait,
+};
+use esp_idf_hal::prelude::Peripherals;
+
 #[cfg(not(feature = "qemu"))]
 use {
-    esp_idf_svc::wifi::{BlockingWifi, EspWifi},
-    esp_idf_sys as _,
+    esp_idf_svc::{
+        wifi::{AsyncWifi, EspWifi},
+    },
     esp_idf_sys::esp_wifi_set_ps,
+    esp_idf_sys as _,
 };
 
-// webhook
-use embedded_svc::{
-    http::{client::Client as HttpClient, Method, Status},
-    io::{Read, Write},
-    utils::io,
-    wifi::{ClientConfiguration, Wifi},
-};
+#[cfg(feature = "qemu")]
+use embedded_svc::ipv4::{ClientSettings, ClientConfiguration, Subnet, Mask};
 
-use esp_idf_svc::{
-    eth::EspEth,
-    http::client::{Configuration, EspHttpConnection},
-    netif::EspNetif, // EspNetifWait
-    //wifi::WifiWait,
-};
-
-use esp_idf_svc::eventloop::EspSystemEventLoop;
-
-use anyhow::bail;
-use log::*;
-use micro_rdk::common::app_client::AppClientConfig;
-use micro_rdk::esp32::certificate::WebRtcCertificate;
-use std::net::Ipv4Addr;
-use std::time::Duration;
-//use futures_lite::future::block_on;
-
-#[cfg(not(feature = "qemu"))]
-use esp_idf_svc::wifi::EspWifi;
-
-// webhook
-use embedded_svc::{
-    http::{client::Client as HttpClient, Method, Status},
-    io::{Read, Write},
-    utils::io,
-};
-#[derive(Deserialize, Debug)]
-struct Response {
-    response: String,
-}
-
-use esp_idf_svc::http::client::{Configuration, EspHttpConnection};
-
-use esp_idf_hal::prelude::Peripherals;
-use micro_rdk::esp32::entry::serve_web;
-use micro_rdk::esp32::tls::Esp32TlsServerConfig;
 
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
 
     esp_idf_svc::log::EspLogger::initialize_default();
     let sys_loop_stack = EspSystemEventLoop::take().unwrap();
-    let periph = esp_idf_hal::prelude::Peripherals::take().unwrap();
+    let periph = Peripherals::take().unwrap();
 
     #[cfg(feature = "qemu")]
     let robot = {
-        use micro_rdk::common::board::FakeBoard;
-        use micro_rdk::common::robot::{LocalRobot, ResourceMap, ResourceType};
-        use micro_rdk::proto::common::v1::ResourceName;
-        use std::collections::HashMap;
-        use std::sync::{Arc, Mutex};
         let board = Arc::new(Mutex::new(FakeBoard::new(vec![])));
         let mut res: ResourceMap = HashMap::with_capacity(1);
         res.insert(
@@ -89,18 +91,35 @@ fn main() -> anyhow::Result<()> {
     #[cfg(not(feature = "qemu"))]
     let robot = None;
 
+
     #[cfg(feature = "qemu")]
-    let (ip, _eth) = {
+    let (ip, _blocking_eth) = {
         info!("creating eth object");
-        let eth = eth_configure(
-            &sys_loop_stack,
-            Box::new(esp_idf_svc::eth::EspEth::wrap(EthDriver::new_openeth(
-                periph.mac,
-                sys_loop_stack.clone(),
-            )?)?),
+
+        let ip =  Ipv4Addr::new(10, 1, 12, 187);
+        let ip_configuration = embedded_svc::ipv4::Configuration::Client(ClientConfiguration::Fixed(ClientSettings{
+            ip,
+            subnet: Subnet {
+                gateway: Ipv4Addr::new(10, 1, 12, 1),
+                mask: Mask(24),
+                },
+            ..Default::default()
+        }));
+
+        let mut eth_config = esp_idf_svc::netif::NetifConfiguration::eth_default_client();
+        // netif_config.custom_mac = Some(periph.mac.into_ref()); // need to get a reference to [u8:6]
+        eth_config.ip_configuration = ip_configuration;
+
+        let netif = esp_idf_svc::netif::EspNetif::new_with_conf(&eth_config)?;
+        let timer = EspTimerService::new()?;
+        // check eth
+        let (ip, eth) = eth_configure(
+    &sys_loop_stack, 
+    netif,
         )?;
-        (Ipv4Addr::new(10, 1, 12, 187), eth)
+        (ip, Box::new(eth))
     };
+
     {
         esp_idf_sys::esp!(unsafe {
             esp_idf_sys::esp_vfs_eventfd_register(&esp_idf_sys::esp_vfs_eventfd_config_t {
@@ -113,15 +132,14 @@ fn main() -> anyhow::Result<()> {
     #[cfg(not(feature = "qemu"))]
     let (ip, _wifi) = {
         let wifi = start_wifi(periph.modem, sys_loop_stack)?;
-        (wifi.sta_netif().get_ip_info()?.ip, wifi)
+        (wifi.wifi().sta_netif().get_ip_info()?.ip, wifi)
     };
 
-    use micro_rdk::common::app_client::AppClientConfig;
     let cfg = AppClientConfig::new(
         ROBOT_SECRET.to_owned(),
         ROBOT_ID.to_owned(),
         ip,
-        "".to_string()
+        "".to_string(),
     );
     let webrtc_certificate =
         WebRtcCertificate::new(ROBOT_DTLS_CERT, ROBOT_DTLS_KEY_PAIR, ROBOT_DTLS_CERT_FP);
@@ -144,14 +162,33 @@ fn main() -> anyhow::Result<()> {
 #[cfg(feature = "qemu")]
 fn eth_configure(
     sl_stack: &EspSystemEventLoop,
-    mut eth: Box<EspEth<'static>>,
-) -> anyhow::Result<Box<EspEth<'static>>> {
-    eth.start()?;
+    eth: EspNetif,
+) -> anyhow::Result<(Ipv4Addr, AsyncNetif<EspNetif>)> {
+    use futures_lite::future::block_on;
 
+    let timer = EspTimerService::new()?;
+    let ip_info = eth.get_ip_info()?;
+    let async_eth = AsyncNetif::wrap(eth, sl_stack.clone(), timer);
+
+    block_on( async {
+            //eth.wifi_wait(|| async_wifi.wifi().is_started(), Some(Duration::from_secs(20)))
+            // .await
+            //.expect("couldn't start wifi");
+            //async_wifi.connect().await.unwrap();
+
+        async_eth.ip_wait_while(
+                || Ok(async_eth.is_up().unwrap()), 
+                Some(Duration::from_secs(20)))
+            .await
+            .expect("ethernet couldn't connect");
+
+    });
+
+    /*
     if !EthWait::new(eth.driver(), sl_stack)?
         .wait_with_timeout(Duration::from_secs(30), || eth.is_started().unwrap())
     {
-        bail!("couldn't start eth driver")
+        anyhow::bail!("couldn't start eth driver")
     }
 
     if !EspNetifWait::new::<EspNetif>(eth.netif(), sl_stack)?
@@ -159,31 +196,56 @@ fn eth_configure(
             eth.netif().get_ip_info().unwrap().ip != Ipv4Addr::new(0, 0, 0, 0)
         })
     {
-        bail!("didn't get an ip")
+        anyhow::bail!("didn't get an ip")
     }
-    let ip_info = eth.netif().get_ip_info()?;
-    info!("ETH IP {:?}", ip_info);
-    Ok(eth)
+        */
+    info!("ETH IP {:?}", ip_info.ip);
+    Ok((ip_info.ip, async_eth))
 }
 
 #[cfg(not(feature = "qemu"))]
 fn start_wifi(
     modem: impl esp_idf_hal::peripheral::Peripheral<P = esp_idf_hal::modem::Modem> + 'static,
     sl_stack: EspSystemEventLoop,
-) -> anyhow::Result<Box<EspWifi<'static>>> {
-    let mut wifi = Box::new(EspWifi::new(modem, sl_stack.clone(), None)?);
+) -> anyhow::Result<AsyncWifi<EspWifi<'static>>> {
+    use futures_lite::future::block_on;
+    use esp_idf_svc::wifi::config::{ScanConfig,ScanType};
+    use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
 
-    info!("scanning");
-    let aps = wifi.scan()?;
-    let foundap = aps.into_iter().find(|x| x.ssid == SSID);
+    let timer = EspTimerService::new()?;
+    let nvs = esp_idf_svc::nvs::EspDefaultNvsPartition::take()?;
+    let mut wifi = AsyncWifi::wrap(EspWifi::new(modem, sl_stack.clone(), Some(nvs))?, sl_stack, timer)?;
+    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+        ssid: SSID.into(),
+        bssid: None,
+        auth_method: AuthMethod::WPA2Personal,
+        password: PASS.into(),
+        channel: None,
+    });
 
-    let channel = if let Some(foundap) = foundap {
-        info!("{} channel is {}", "Viam", foundap.channel);
-        Some(foundap.channel)
-    } else {
-        None
-    };
-    let client_config = ClientConfiguration {
+    wifi.set_configuration(&wifi_configuration)?;
+
+    block_on( async {
+    wifi.start().await.unwrap();
+    info!("Wifi started");
+
+    wifi.connect().await.unwrap();
+    info!("Wifi connected");
+
+    wifi.wait_netif_up().await.unwrap();
+    info!("Wifi netif up");
+
+    });
+
+    /*
+    info!("mattjperez - W");
+    let ap = wifi.start_scan(&scan_conf, false)?;
+    info!("mattjperez - Z");
+    let ap = &wifi.get_scan_result_n::<1>()?.0[0];
+    info!("{} channel is {}", "Viam", ap.channel);
+    let channel = Some(ap.channel);
+    info!("mattjperez - X");
+    let client_config = embedded_svc::wifi::ClientConfiguration {
         ssid: SSID.into(),
         password: PASS.into(),
         channel,
@@ -193,93 +255,38 @@ fn start_wifi(
 
     wifi.start()?;
 
-    /*   // refactor to use updated api
-    if !WifiWait::new(&sl_stack)?
-        .wait_with_timeout(Duration::from_secs(20), || wifi.is_started().unwrap())
-    {
-        bail!("couldn't start wifi")
-    }
-        */
+    info!("mattjperez - A");
+    let mut async_wifi = AsyncWifi::wrap(wifi, sl_stack.clone(), timer)?;
+    info!("mattjperez - B");
+*/
 
-    wifi.connect()?;
+    /*
+    block_on(async {
+        async_wifi
+            .wifi_wait(|| async_wifi.wifi().is_started(), Some(Duration::from_secs(20)))
+            .await
+            .expect("couldn't start wifi");
+            async_wifi.connect().await.unwrap();
+    info!("mattjperez - C");
 
-    /*   // refactor to use updated api
-    if !EspNetifWait::new::<EspNetif>(wifi.sta_netif(), &sl_stack)?.wait_with_timeout( 
-        Duration::from_secs(20),
-        || {
-            wifi.is_connected().unwrap()
-                && wifi.sta_netif().get_ip_info().unwrap().ip != Ipv4Addr::new(0, 0, 0, 0)
-        },
-    ) {
-        bail!("wifi couldn't connect")
-    }
-        */
+        async_wifi
+            .ip_wait_while(
+                || Ok(async_wifi.wifi().is_connected().unwrap() && async_wifi.wifi().sta_netif().get_ip_info().unwrap().ip != Ipv4Addr::new(0,0,0,0)), 
+                Some(Duration::from_secs(20)))
+            .await
+            .expect("wifi couldn't connect");
+            async_wifi.connect().await.unwrap();
+    info!("mattjperez - D");
+    });
 
-    let ip_info = wifi.sta_netif().get_ip_info()?;
+    info!("mattjperez - E");
+    let ip_info = async_wifi.wifi().sta_netif().get_ip_info()?;
 
     info!("Wifi DHCP info: {:?}", ip_info);
 
     esp_idf_sys::esp!(unsafe { esp_wifi_set_ps(esp_idf_sys::wifi_ps_type_t_WIFI_PS_NONE) })?;
 
+    */
     Ok(wifi)
 }
-#[derive(Deserialize, Debug)]
-struct Response {
-    response: String,
-}
 
-/// Send a HTTP GET request.
-fn get_request(client: &mut HttpClient<EspHttpConnection>) -> anyhow::Result<()> {
-    // Prepare headers and URL
-    //let content_length_header = format!("{}", payload.len());
-
-    let payload = json!({
-        /*
-        "location": "<ROBOT LOCATION>",
-        "secret": "<ROBOT SECRET>",
-        "target": "<COMPONENT BOARD NAME>",
-        "pin": pin-no
-        */
-        "delete": "this"
-    })
-    .to_string();
-    let payload = payload.as_bytes();
-    // Prepare headers and URL
-    let content_length_header = format!("{}", payload.len());
-    let headers = [
-        ("accept", "text/plain"),
-        ("content-type", "application/json"),
-        ("connection", "close"),
-        ("content-length", &*content_length_header),
-    ];
-    let url = "https://restless-shape-1762.fly.dev/esp";
-
-    // Send request
-    //let mut request = client.get(&url, &headers)?;
-    let mut request = client.request(Method::Get, &url, &headers)?;
-    request.write_all(payload)?;
-    request.flush()?;
-    info!("-> GET {}", url);
-    let mut response = request.submit()?;
-
-    // Process response
-    let status = response.status();
-    info!("<- {}", status);
-    let (_headers, mut body) = response.split();
-    let mut buf = [0u8; 4096];
-    let bytes_read = io::try_read_full(&mut body, &mut buf).map_err(|e| e.0)?;
-    info!("Read {} bytes", bytes_read);
-    let response: Response = serde_json::from_slice(&buf[0..bytes_read])?;
-    info!("Response body: {:?} bytes", response);
-
-    // Drain the remaining response bytes
-    while body.read(&mut buf)? > 0 {}
-
-    //let bytes_read = io::try_read_full(&mut body, &mut buf).map_err(|e| e.0)?;
-    //info!("Read {} bytes", bytes_read);
-
-    // Drain the remaining response bytes
-    //while body.read(&mut buf)? > 0 {}
-
-    Ok(())
-}
