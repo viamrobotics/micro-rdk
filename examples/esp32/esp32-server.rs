01d5add4eff7df0a9e9a3bc4526fb343aa428a9a
Matthew J Perez <matt@mperez.io>
2023-06-22 17:07:51 -0400

    working updated wifi example

--- examples/esp32/esp32-server.rs
+++ examples/esp32/esp32-server.rs
@@ -171,34 +171,13 @@ fn eth_configure(
     let async_eth = AsyncNetif::wrap(eth, sl_stack.clone(), timer);
 
     block_on( async {
-            //eth.wifi_wait(|| async_wifi.wifi().is_started(), Some(Duration::from_secs(20)))
-            // .await
-            //.expect("couldn't start wifi");
-            //async_wifi.connect().await.unwrap();
-
         async_eth.ip_wait_while(
                 || Ok(async_eth.is_up().unwrap()), 
                 Some(Duration::from_secs(20)))
             .await
             .expect("ethernet couldn't connect");
-
     });
 
-    /*
-    if !EthWait::new(eth.driver(), sl_stack)?
-        .wait_with_timeout(Duration::from_secs(30), || eth.is_started().unwrap())
-    {
-        anyhow::bail!("couldn't start eth driver")
-    }
-
-    if !EspNetifWait::new::<EspNetif>(eth.netif(), sl_stack)?
-        .wait_with_timeout(Duration::from_secs(20), || {
-            eth.netif().get_ip_info().unwrap().ip != Ipv4Addr::new(0, 0, 0, 0)
-        })
-    {
-        anyhow::bail!("didn't get an ip")
-    }
-        */
     info!("ETH IP {:?}", ip_info.ip);
     Ok((ip_info.ip, async_eth))
 }
@@ -207,7 +186,7 @@ fn eth_configure(
 fn start_wifi(
     modem: impl esp_idf_hal::peripheral::Peripheral<P = esp_idf_hal::modem::Modem> + 'static,
     sl_stack: EspSystemEventLoop,
-) -> anyhow::Result<AsyncWifi<EspWifi<'static>>> {
+) -> anyhow::Result<Box<AsyncWifi<EspWifi<'static>>>> {
     use futures_lite::future::block_on;
     use esp_idf_svc::wifi::config::{ScanConfig,ScanType};
     use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
@@ -237,56 +216,7 @@ fn start_wifi(
 
     });
 
-    /*
-    info!("mattjperez - W");
-    let ap = wifi.start_scan(&scan_conf, false)?;
-    info!("mattjperez - Z");
-    let ap = &wifi.get_scan_result_n::<1>()?.0[0];
-    info!("{} channel is {}", "Viam", ap.channel);
-    let channel = Some(ap.channel);
-    info!("mattjperez - X");
-    let client_config = embedded_svc::wifi::ClientConfiguration {
-        ssid: SSID.into(),
-        password: PASS.into(),
-        channel,
-        ..Default::default()
-    };
-    wifi.set_configuration(&embedded_svc::wifi::Configuration::Client(client_config))?;
-
-    wifi.start()?;
-
-    info!("mattjperez - A");
-    let mut async_wifi = AsyncWifi::wrap(wifi, sl_stack.clone(), timer)?;
-    info!("mattjperez - B");
-*/
-
-    /*
-    block_on(async {
-        async_wifi
-            .wifi_wait(|| async_wifi.wifi().is_started(), Some(Duration::from_secs(20)))
-            .await
-            .expect("couldn't start wifi");
-            async_wifi.connect().await.unwrap();
-    info!("mattjperez - C");
-
-        async_wifi
-            .ip_wait_while(
-                || Ok(async_wifi.wifi().is_connected().unwrap() && async_wifi.wifi().sta_netif().get_ip_info().unwrap().ip != Ipv4Addr::new(0,0,0,0)), 
-                Some(Duration::from_secs(20)))
-            .await
-            .expect("wifi couldn't connect");
-            async_wifi.connect().await.unwrap();
-    info!("mattjperez - D");
-    });
-
-    info!("mattjperez - E");
-    let ip_info = async_wifi.wifi().sta_netif().get_ip_info()?;
-
-    info!("Wifi DHCP info: {:?}", ip_info);
-
     esp_idf_sys::esp!(unsafe { esp_wifi_set_ps(esp_idf_sys::wifi_ps_type_t_WIFI_PS_NONE) })?;
-
-    */
-    Ok(wifi)
+    Ok(Box::new(wifi))
 }
 
