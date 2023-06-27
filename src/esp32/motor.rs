ae4bf8d61153b9c7a61b0a12f1df42ce3ba55894
Gautham <gautham@viam.com>
2023-06-27 14:11:12 -0400

    RSDK-2322 - support configuration for PWM-Direction pin motors

--- src/esp32/motor.rs
+++ src/esp32/motor.rs
@@ -1,16 +1,16 @@
 //! Base structs and methods for supported motors.
 //!
-//! # Creating a `PwmDirMotorEsp32` Motor
+//! # Creating a `PwmDirectionMotorEsp32` Motor
 //!
 //! ```ignore
 //! use esp_idf_hal::ledc::{config::TimerConfig, LedcDriver, LedcTimerDriver};
-//! use micro_rdk::esp32::motor::PwmDirMotorEsp32;
+//! use micro_rdk::esp32::motor::PwmDirectionMotorEsp32;
 //! use esp_idf_hal::units::FromValueType;
 //!
 //! let tconf = TimerConfig::default().frequency(10.kHz().into());
 //! let timer = Arc::new(LedcTimerDriver::new(periph.ledc.channel0, &tconf)).unwrap();
 //! let pwm = LedcDriver::new(periph.ledc.channel0, timer.clone(), periph.pins.gpio15)?;
-//! let mut motor = PwmDirMotorEsp32::new(
+//! let mut motor = PwmDirectionMotorEsp32::new(
 //!     PinDriver::output(periph.pins.gpio23)?, // DIR
 //!     pwm, // PWM
 //!     true, // dir_flip
@@ -49,7 +49,9 @@ use crate::common::encoder::{
     Encoder, EncoderPositionType, EncoderType, COMPONENT_NAME as EncoderCompName,
 };
 use crate::common::math_utils::go_for_math;
-use crate::common::motor::{Motor, MotorPinsConfig, MotorType, COMPONENT_NAME as MotorCompName};
+use crate::common::motor::{
+    Motor, MotorPinType, MotorPinsConfig, MotorType, COMPONENT_NAME as MotorCompName,
+};
 use crate::common::registry::{ComponentRegistry, Dependency, ResourceKey};
 use crate::common::robot::Resource;
 use crate::common::status::Status;
@@ -86,13 +88,26 @@ pub(crate) fn register_models(registry: &mut ComponentRegistry) {
 }
 
 // Generates a motor or an encoded motor depending on whether an encoder has been added as
-// a dependency from the config. TODO: Add support for initializing PwmDirMotorEsp32.
+// a dependency from the config. TODO: Add support for initializing PwmDirectionMotorEsp32.
 pub fn gpio_motor_from_config(cfg: ConfigType, deps: Vec<Dependency>) -> anyhow::Result<MotorType> {
-    let motor = ABMotorEsp32::<
-        PinDriver<'_, AnyOutputPin, Output>,
-        PinDriver<'_, AnyOutputPin, Output>,
-        LedcDriver<'_>,
-    >::from_config(cfg)?;
+    let motor_type = if let Ok(pin_cfg) = cfg.get_attribute::<MotorPinsConfig>("pins") {
+        pin_cfg.detect_motor_type()?
+    } else {
+        return Err(anyhow::anyhow!("pin parameters for motor not found"));
+    };
+    let motor = match motor_type {
+        MotorPinType::PwmAB => ABMotorEsp32::<
+            PinDriver<'_, AnyOutputPin, Output>,
+            PinDriver<'_, AnyOutputPin, Output>,
+            LedcDriver<'_>,
+        >::from_config(cfg)?
+        .clone(),
+        MotorPinType::PwmDirection => PwmDirectionMotorEsp32::<
+            PinDriver<'_, AnyOutputPin, Output>,
+            LedcDriver<'_>,
+        >::from_config(cfg)?
+        .clone(),
+    };
     let mut enc: Option<EncoderType> = None;
     for Dependency(_, dep) in deps {
         match dep {
@@ -185,6 +200,7 @@ pub struct ABMotorEsp32<A, B, PWM> {
     b: B,
     pwm: PWM,
     max_rpm: f64,
+    dir_flip: bool,
 }
 
 impl<A, B, PWM> ABMotorEsp32<A, B, PWM>
@@ -193,8 +209,14 @@ where
     B: OutputPin + PinExt,
     PWM: PwmPin<Duty = u32>,
 {
-    pub fn new(a: A, b: B, pwm: PWM, max_rpm: f64) -> Self {
-        ABMotorEsp32 { a, b, pwm, max_rpm }
+    pub fn new(a: A, b: B, pwm: PWM, max_rpm: f64, dir_flip: bool) -> Self {
+        ABMotorEsp32 {
+            a,
+            b,
+            pwm,
+            max_rpm,
+            dir_flip,
+        }
     }
 
     pub(crate) fn dependencies_from_config(cfg: ConfigType) -> Vec<ResourceKey> {
@@ -221,11 +243,17 @@ where
                     PwmChannel::C2(c) => LedcDriver::new(c, timer, pwm_pin)?,
                 };
                 let max_rpm: f64 = cfg.get_attribute::<f64>("max_rpm")?;
+                let dir_flip: bool = if let Ok(dir_flip) = cfg.get_attribute::<bool>("dir_flip") {
+                    dir_flip
+                } else {
+                    false
+                };
                 return Ok(Arc::new(Mutex::new(ABMotorEsp32::new(
                     PinDriver::output(unsafe { AnyOutputPin::new(pins.a.unwrap()) })?,
                     PinDriver::output(unsafe { AnyOutputPin::new(pins.b.unwrap()) })?,
                     chan,
                     max_rpm,
+                    dir_flip,
                 ))));
             }
         }
@@ -244,21 +272,23 @@ where
             anyhow::bail!("power outside limit")
         }
         let max_duty = self.pwm.get_max_duty();
-        if pct < 0.0 {
+        let set_forwards = (pct > 0.0) && !self.dir_flip;
+        if set_forwards {
             self.a
-                .set_high()
+                .set_low()
                 .map_err(|_| anyhow::anyhow!("error setting A pin"))?;
             self.b
-                .set_low()
+                .set_high()
                 .map_err(|_| anyhow::anyhow!("error setting B pin"))?;
         } else {
             self.a
-                .set_low()
+                .set_high()
                 .map_err(|_| anyhow::anyhow!("error setting A pin"))?;
             self.b
-                .set_high()
+                .set_low()
                 .map_err(|_| anyhow::anyhow!("error setting B pin"))?;
         }
+
         self.pwm
             .set_duty(((max_duty as f64) * pct.abs()).floor() as u32);
         debug!("set to {:?} pct", pct);
@@ -308,14 +338,14 @@ where
 }
 
 // Represents a motor using a direction pin and a PWM pin
-pub struct PwmDirMotorEsp32<DIR, PWM> {
+pub struct PwmDirectionMotorEsp32<DIR, PWM> {
     dir: DIR,
     pwm: PWM,
     dir_flip: bool,
     max_rpm: f64,
 }
 
-impl<DIR, PWM> PwmDirMotorEsp32<DIR, PWM>
+impl<DIR, PWM> PwmDirectionMotorEsp32<DIR, PWM>
 where
     DIR: OutputPin + PinExt,
     PWM: PwmPin<Duty = u32>,
@@ -328,9 +358,40 @@ where
             max_rpm,
         }
     }
+
+    pub(crate) fn from_config(cfg: ConfigType) -> anyhow::Result<MotorType> {
+        if let Ok(pins) = cfg.get_attribute::<MotorPinsConfig>("pins") {
+            if pins.dir.is_some() {
+                use esp_idf_hal::units::FromValueType;
+                let pwm_tconf = TimerConfig::default().frequency(10.kHz().into());
+                let timer =
+                    LedcTimerDriver::new(unsafe { esp_idf_hal::ledc::TIMER0::new() }, &pwm_tconf)?;
+                let pwm_pin = unsafe { AnyOutputPin::new(pins.pwm) };
+                let chan = PWMCHANNELS.lock().unwrap().take_next_channel()?;
+                let chan = match chan {
+                    PwmChannel::C0(c) => LedcDriver::new(c, timer, pwm_pin)?,
+                    PwmChannel::C1(c) => LedcDriver::new(c, timer, pwm_pin)?,
+                    PwmChannel::C2(c) => LedcDriver::new(c, timer, pwm_pin)?,
+                };
+                let max_rpm: f64 = cfg.get_attribute::<f64>("max_rpm")?;
+                let dir_flip: bool = if let Ok(dir_flip) = cfg.get_attribute::<bool>("dir_flip") {
+                    dir_flip
+                } else {
+                    false
+                };
+                return Ok(Arc::new(Mutex::new(PwmDirectionMotorEsp32::new(
+                    PinDriver::output(unsafe { AnyOutputPin::new(pins.dir.unwrap()) })?,
+                    chan,
+                    dir_flip,
+                    max_rpm,
+                ))));
+            }
+        }
+        Err(anyhow::anyhow!("cannot build motor"))
+    }
 }
 
-impl<DIR, PWM> Motor for PwmDirMotorEsp32<DIR, PWM>
+impl<DIR, PWM> Motor for PwmDirectionMotorEsp32<DIR, PWM>
 where
     DIR: OutputPin + PinExt,
     PWM: PwmPin<Duty = u32>,
@@ -368,7 +429,7 @@ where
     }
 }
 
-impl<DIR, PWM> Status for PwmDirMotorEsp32<DIR, PWM>
+impl<DIR, PWM> Status for PwmDirectionMotorEsp32<DIR, PWM>
 where
     DIR: OutputPin + PinExt,
     PWM: PwmPin<Duty = u32>,
@@ -386,7 +447,7 @@ where
     }
 }
 
-impl<DIR, PWM> Stoppable for PwmDirMotorEsp32<DIR, PWM>
+impl<DIR, PWM> Stoppable for PwmDirectionMotorEsp32<DIR, PWM>
 where
     DIR: OutputPin + PinExt,
     PWM: PwmPin<Duty = u32>,
