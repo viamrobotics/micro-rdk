2431cb4e6fc59f971a9b674f8d16cc33203a5ba0
Nicolas Menard <nicolas.p.menard@gmail.com>
2023-06-30 13:59:54 -0400

    remove signaling test

--- src/native/entry.rs
+++ src/native/entry.rs
@@ -71,30 +71,19 @@ pub fn serve_web(
 #[cfg(test)]
 mod tests {
     use crate::common::app_client::{AppClientBuilder, AppClientConfig};
-    use crate::common::board::FakeBoard;
-    use crate::common::grpc::GrpcServer;
-    use crate::common::grpc_client::GrpcClient;
-    use crate::common::robot::{LocalRobot, ResourceMap, ResourceType};
-    use crate::common::webrtc::grpc::{WebRtcGrpcBody, WebRtcGrpcServer};
-    use crate::native::certificate::WebRtcCertificate;
 
-    use crate::native::dtls::Dtls;
+    use crate::common::grpc_client::GrpcClient;
+
     use crate::native::exec::NativeExecutor;
     use crate::native::tcp::NativeStream;
     use crate::native::tls::NativeTls;
 
-    use crate::proto::common::v1::ResourceName;
     use crate::proto::rpc::examples::echo::v1::{EchoBiDiRequest, EchoBiDiResponse};
 
     use futures_lite::future::block_on;
     use futures_lite::StreamExt;
-    use local_ip_address::local_ip;
 
-    use std::collections::HashMap;
-    use std::net::{IpAddr, Ipv4Addr, TcpStream};
-
-    use std::rc::Rc;
-    use std::sync::{Arc, Mutex};
+    use std::net::{Ipv4Addr, TcpStream};
 
     #[test_log::test]
     #[ignore]
@@ -197,65 +186,5 @@ mod tests {
 
         assert_eq!("123456", p);
         Ok(())
-    }
-
-    #[test_log::test]
-    #[ignore]
-    fn test_webrtc_signaling() -> anyhow::Result<()> {
-        let our_ip = match local_ip()? {
-            IpAddr::V4(v4) => v4,
-            _ => panic!("didn't get an ipv4"),
-        };
-        let cfg = AppClientConfig::new("".to_string(), "".to_string(), our_ip, "".to_owned());
-
-        let robot = {
-            let board = Arc::new(Mutex::new(FakeBoard::new(vec![])));
-            let mut res: ResourceMap = HashMap::with_capacity(1);
-            res.insert(
-                ResourceName {
-                    namespace: "rdk".to_string(),
-                    r#type: "component".to_string(),
-                    subtype: "board".to_string(),
-                    name: "b".to_string(),
-                },
-                ResourceType::Board(board),
-            );
-            Arc::new(Mutex::new(LocalRobot::new(res)))
-        };
-
-        let executor = NativeExecutor::new();
-
-        let mut webrtc = {
-            let executor = executor.clone();
-            let tls = Box::new(NativeTls::new_client());
-            let conn = tls.open_ssl_context(None)?;
-            let conn = NativeStream::TLSStream(Box::new(conn));
-            let grpc_client = Box::new(GrpcClient::new(
-                conn,
-                executor.clone(),
-                "https://app.viam.com:443",
-            )?);
-            let mut app_client = AppClientBuilder::new(grpc_client, cfg).build()?;
-
-            let cert = Rc::new(WebRtcCertificate::new()?);
-
-            let dtls = Dtls::new(cert.clone())?;
-
-            let webrtc = app_client
-                .connect_webrtc(cert, executor.clone(), dtls)
-                .unwrap();
-
-            drop(app_client);
-
-            webrtc
-        };
-        let channel = block_on(executor.run(async { webrtc.open_data_channel().await })).unwrap();
-
-        let mut webrtc_grpc =
-            WebRtcGrpcServer::new(channel, GrpcServer::new(robot, WebRtcGrpcBody::default()));
-
-        loop {
-            block_on(executor.run(async { webrtc_grpc.next_request().await.unwrap() }));
-        }
     }
 }
