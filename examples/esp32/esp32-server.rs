2a5334908920ce341ce366564eb56d016d2c4d86
Matthew J Perez <matt@mperez.io>
2023-08-01 15:50:12 -0400

    fmt

--- examples/esp32/esp32-server.rs
+++ examples/esp32/esp32-server.rs
@@ -11,7 +11,7 @@ use log::*;
 
 use esp_idf_svc::eventloop::EspSystemEventLoop;
 use micro_rdk::{
-    common::{registry::ComponentRegistry, app_client::AppClientConfig, robot::Initializer},
+    common::{app_client::AppClientConfig, registry::ComponentRegistry robot::Initializer},
     esp32::{certificate::WebRtcCertificate, entry::serve_web, tls::Esp32TlsServerConfig},
 };
 
