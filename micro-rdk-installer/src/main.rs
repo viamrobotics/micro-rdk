91e7c0a63c7424bc3a30fa23118e25476859ca7c
Gautham <gautham@viam.com>
2023-08-31 13:43:13 -0400

    arg adjustment

--- micro-rdk-installer/src/main.rs
+++ micro-rdk-installer/src/main.rs
@@ -35,7 +35,7 @@ enum Commands {
 struct WriteCredentials {
     #[arg(long = "app-config")]
     config: String,
-    #[arg(long = "binary-path", short = "bin")]
+    #[arg(long = "app-binary")]
     binary_path: String,
     #[arg(long = "nvs-size", default_value = "32768")]
     nvs_size: usize,
