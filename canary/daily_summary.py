3a46da1dcd6c85547c990bd0b73a4a4eab264f59
Gautham <gautham@viam.com>
2024-04-19 11:43:53 -0400

    post daily failure rate to slack channel

--- canary/daily_summary.py
+++ canary/daily_summary.py
@@ -1,8 +1,11 @@
 import datetime
 import os
 
+from importlib.metadata import version
 from dateutil import tz
 from pymongo import MongoClient
+import slack_sdk
+import slack_sdk.errors
 
 FAILURE_ACCEPTABILITY = 0.2
 
@@ -45,7 +48,7 @@ def main():
     avg_connection_attempts = connection_attempts / num_results
     
     total_board_calls = board_api_successes + board_api_failures
-
+    sdk_version = version("viam-sdk")
     coll2 = db["daily_summaries"]
     summary = {
         "_id": start_of_day,
@@ -53,18 +56,36 @@ def main():
         "robot_connection_failures": connection_failures,
         "board_api_failures": board_api_failures,
         "avg_connection_latency_ms": avg_connection_latency_ms,
-        "avg_connection_attempts": avg_connection_attempts
+        "avg_connection_attempts": avg_connection_attempts,
+        "sdk_version": sdk_version
     }
     inserted_id = coll2.insert_one(summary)
     print(f"successfully inserted stats for {inserted_id}: {summary}")
 
     failure_rate = round(connection_failures / num_results, 3)
+
+    slack_token = os.environ["CANARY_SLACKBOT_TOKEN"]
+    channel_id = os.environ["MICRO_RDK_TEAM_CHANNEL_ID"]
+    client = slack_sdk.WebClient(token=slack_token)
+    version_msg = f"using Viam Python SDK {sdk_version}"
     if failure_rate > FAILURE_ACCEPTABILITY:
-        raise Exception(f"Connection failure rate {failure_rate * 100}%% greater than {FAILURE_ACCEPTABILITY * 100}%%")
+        msg = f"ESP32 connection failure rate for {today} ({failure_rate * 100}%) greater than {FAILURE_ACCEPTABILITY * 100}% ({version_msg})"
+        api_result = client.chat_postMessage(channel=channel_id, text=msg)
+        try:
+            api_result.validate()
+            raise Exception(msg)
+        except slack_sdk.errors.SlackApiError as e:
+            raise Exception(f"failure to post to Slack, error message was '{msg}'") from e
     
     board_failure_rate = round(board_api_failures / total_board_calls)
     if board_failure_rate > FAILURE_ACCEPTABILITY:
-        raise Exception(f"Connection failure rate {failure_rate * 100}%% greater than {FAILURE_ACCEPTABILITY * 100}%%")
+        msg = f"ESP32 board API failure rate for {today} ({board_failure_rate * 100}%) greater than {FAILURE_ACCEPTABILITY * 100}% ({version_msg})"
+        api_result = client.chat_postMessage(channel=channel_id, text=msg)
+        try:
+            api_result.validate()
+            raise Exception(msg)
+        except slack_sdk.errors.SlackApiError as e:
+            raise Exception(f"failure to post to Slack, error message was '{msg}'") from e
 
 if __name__ == '__main__':
     main()
