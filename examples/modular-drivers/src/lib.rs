b38a085a665ee578d1ef18a751185fd30fe05a81
Allison Chiang <83091926+allisonschiang@users.noreply.github.com>
2024-06-17 14:16:58 -0400

    consolidate cargo.toml, lib.rs, and add into workspace

--- examples/modular-drivers/src/lib.rs
+++ examples/modular-drivers/src/lib.rs
@@ -2,9 +2,13 @@ use micro_rdk::common::registry::{ComponentRegistry, RegistryError};
 
 pub mod free_heap_sensor;
 pub mod wifi_rssi_sensor;
+pub mod moisture_sensor;
+pub mod water_pump;
 
 pub fn register_models(registry: &mut ComponentRegistry) -> Result<(), RegistryError> {
     wifi_rssi_sensor::register_models(registry)?;
     free_heap_sensor::register_models(registry)?;
+    moisture_sensor::register_models(registry)?;
+    water_pump::register_models(registry)?;
     Ok(())
 }
