155e4523b1f9a08c6160f2f8cfd8682e3a86bf19
Allison Chiang <83091926+allisonschiang@users.noreply.github.com>
2024-06-18 13:58:52 -0400

    more feature gating

--- examples/modular-drivers/src/lib.rs
+++ examples/modular-drivers/src/lib.rs
@@ -8,9 +8,11 @@ pub mod water_pump;
 pub mod wifi_rssi_sensor;
 
 pub fn register_models(registry: &mut ComponentRegistry) -> Result<(), RegistryError> {
+    #[cfg(feature = "esp32")]
     free_heap_sensor::register_models(registry)?;
     moisture_sensor::register_models(registry)?;
     water_pump::register_models(registry)?;
+    #[cfg(feature = "esp32")]
     wifi_rssi_sensor::register_models(registry)?;
     Ok(())
 }
