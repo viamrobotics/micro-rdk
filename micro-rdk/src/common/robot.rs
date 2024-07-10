d0598bb803a695a8d8138d76cc63bef2e0bfc7f8
Nicolas Menard <nicolas.p.menard@gmail.com>
2024-07-10 15:40:24 -0400

    [RSDK-8131] Avoid killing the collector task on any error (#256)

--- micro-rdk/src/common/robot.rs
+++ micro-rdk/src/common/robot.rs
@@ -287,9 +287,7 @@ impl LocalRobot {
                     let _ = robot
                         .data_manager_collection_task
                         .replace(robot.executor.spawn(async move {
-                            if let Err(err) = data_manager.data_collection_task().await {
-                                log::error!("error running data manager: {:?}", err)
-                            }
+                            data_manager.data_collection_task().await;
                         }));
                 }
                 Ok(None) => {}
