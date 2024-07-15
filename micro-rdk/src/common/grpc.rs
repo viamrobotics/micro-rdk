34d407f918681abeb53c5b3a16dec7885647f79f
Allison Chiang <83091926+allisonschiang@users.noreply.github.com>
2024-07-15 11:46:15 -0400

    update

--- micro-rdk/src/common/grpc.rs
+++ micro-rdk/src/common/grpc.rs
@@ -1214,13 +1214,13 @@ where
     }
 
     //cannot guarantee a response if shutdown is successful since robot will be restarting
-    fn robot_shutdown(&mut self, _: &[u8]) -> Result<(), ServerError> {
+    fn robot_shutdown(&mut self, _: &[u8]) -> ! {
         #[cfg(feature = "native")]
         std::process::exit(0);
         #[cfg(feature = "esp32")]
-        crate::esp32::esp_idf_svc::sys::esp_restart()
-            .take()
-            .unwrap();
+        unsafe {
+            crate::esp32::esp_idf_svc::sys::esp_restart();
+        }
     }
 
     fn robot_status(&mut self, message: &[u8]) -> Result<(), ServerError> {
