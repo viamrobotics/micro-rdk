44ff3f32193b400646e887d75c66c8659e866373
Matthew J Perez <matt@mperez.io>
2024-07-31 18:40:52 -0400

    update project template

--- templates/project/src/main.rs
+++ templates/project/src/main.rs
@@ -1,14 +1,19 @@
-use micro_rdk::esp32::esp_idf_svc::sys::{g_wifi_feature_caps, CONFIG_FEATURE_CACHE_TX_BUF_BIT};
 use micro_rdk::{
     common::{
         entry::RobotRepresentation,
+        provisioning::server::ProvisioningInfo,
         registry::{ComponentRegistry, RegistryError},
     },
-    esp32::entry::serve_web,
+    esp32::{
+        entry::serve_web,
+        esp_idf_svc::{
+            log::EspLogger,
+            sys::{self, esp, g_wifi_feature_caps, CONFIG_FEATURE_CACHE_TX_BUF_BIT},
+        },
+        nvs_storage::NVSStorage,
+    },
 };
 
-use micro_rdk::common::provisioning::server::ProvisioningInfo;
-use micro_rdk::esp32::nvs_storage::NVSStorage;
 macro_rules! generate_register_modules {
     ($($module:ident),*) => {
         #[allow(unused_variables)]
@@ -29,25 +34,39 @@ extern "C" {
 }
 
 fn main() {
-    micro_rdk::esp32::esp_idf_svc::sys::link_patches();
+    sys::link_patches();
+    EspLogger::initialize_default();
 
-    micro_rdk::esp32::esp_idf_svc::log::EspLogger::initialize_default();
-
-    {
-        micro_rdk::esp32::esp_idf_svc::sys::esp!(unsafe {
-            micro_rdk::esp32::esp_idf_svc::sys::esp_vfs_eventfd_register(
-                &micro_rdk::esp32::esp_idf_svc::sys::esp_vfs_eventfd_config_t { max_fds: 5 },
-            )
-        })
+    esp!(unsafe { sys::esp_vfs_eventfd_register(&sys::esp_vfs_eventfd_config_t { max_fds: 5 },) })
         .unwrap();
-    }
 
-    let mut max_connection = 3;
-    unsafe {
+    let max_connections = unsafe {
         if !g_spiram_ok {
             log::info!("spiram not initialized disabling cache feature of the wifi driver");
             g_wifi_feature_caps &= !(CONFIG_FEATURE_CACHE_TX_BUF_BIT as u64);
-            max_connection = 1;
+            1
+        } else {
+            3
+        }
+    };
+
+    let storage = NVSStorage::new("nvs").unwrap();
+
+    let info = if cfg!(feature = "provisioning") {
+        let mut info = ProvisioningInfo::default();
+        info.set_manufacturer("viam".to_owned());
+        info.set_model("esp32".to_owned());
+        Some(info)
+    } else {
+        None
+    };
+
+    if info.is_none() && !storage.has_wifi_credentials() {
+        log::error!("device in an unusable state");
+        log::warning!("enable the `provisioning` feature or build with wifi credentials");
+        log::error!("sleeping indefinitely...");
+        unsafe {
+            sys::esp_deep_sleep_start();
         }
     }
 
@@ -56,9 +75,5 @@ fn main() {
         log::error!("couldn't register modules {:?}", e);
     }
     let repr = RobotRepresentation::WithRegistry(registry);
-    let mut info = ProvisioningInfo::default();
-    info.set_manufacturer("viam".to_owned());
-    info.set_model("esp32".to_owned());
-    let storage = NVSStorage::new("nvs").unwrap();
-    serve_web(Some(info), repr, max_connection, storage);
+    serve_web(info, repr, max_connections, storage);
 }
