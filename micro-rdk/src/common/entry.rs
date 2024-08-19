a281b0b484e4c9380ba852076cecd2d9d687ec61
Matthew J Perez <matt@mperez.io>
2024-08-19 14:47:36 -0500

    handle get_app_config error case

--- micro-rdk/src/common/entry.rs
+++ micro-rdk/src/common/entry.rs
@@ -100,7 +100,10 @@ pub async fn serve_inner<S>(
     let (cfg_response, cfg_received_datetime) = app_client
         .get_app_config(Some(network.get_ip()))
         .await
-        .unwrap();
+        .unwrap_or_else(|e| {
+            log::error!("failed to `get_app_config`: {}", e.to_string());
+            (Box::new(ConfigResponse::default()), None)
+        });
 
     let rpc_host = cfg_response
         .config
