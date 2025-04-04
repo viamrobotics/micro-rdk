0451e65c6dff6d51a3291f08299178aad0f14521
Matthew J Perez <matt@mperez.io>
2025-04-04 14:35:34 -0400

    don't reset on native

--- micro-rdk/src/common/config_monitor.rs
+++ micro-rdk/src/common/config_monitor.rs
@@ -150,6 +150,7 @@ where
                 }
             }
 
+            #[cfg(feature="esp32")]
             if reboot {
                 log::info!("rebooting from config monitor...");
                 // TODO(RSDK-9464): flush logs to app.viam before restarting
