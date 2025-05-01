05ac095873163a318238924de91a1a6cd037dcdf
Matthew J Perez <matt@mperez.io>
2025-05-01 12:20:53 -0500

    fmt

--- micro-rdk/src/esp32/conn/network.rs
+++ micro-rdk/src/esp32/conn/network.rs
@@ -275,8 +275,7 @@ impl Esp32WifiNetwork {
         &self,
         mut networks: Vec<NetworkSetting>,
     ) -> Result<(), WifiManagerError> {
-        self.set_station_mode(NetworkSetting::default())
-            .await?;
+        self.set_station_mode(NetworkSetting::default()).await?;
         self.start().await?;
 
         log::info!("scanning available networks");
